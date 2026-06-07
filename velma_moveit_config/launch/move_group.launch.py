62d6dfabaf51a828acc68d3a04b6d9a0a8807864
dseredyn <dawid.seredynski@gmail.com>
2026-06-07 18:04:50 +0200

    added new gripper mount

--- velma_moveit_config/launch/move_group.launch.py
+++ velma_moveit_config/launch/move_group.launch.py
@@ -1,7 +1,89 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_move_group_launch
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+from moveit_configs_utils import MoveItConfigsBuilder
+from ament_index_python.packages import get_package_share_directory
+import os
+import yaml
+
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    with open(absolute_file_path, "r") as f:
+        return yaml.safe_load(f)
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("velma", package_name="velma_moveit_config").to_moveit_configs()
-    return generate_move_group_launch(moveit_config)
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    moveit_config = (
+        MoveItConfigsBuilder("velma", package_name="velma_moveit_config")
+        .trajectory_execution(
+            file_path="config/moveit_controllers.yaml",
+            moveit_manage_controllers=False,
+        )
+        .planning_scene_monitor(
+            publish_planning_scene=True,
+            publish_geometry_updates=True,
+            publish_state_updates=True,
+            publish_transforms_updates=True,
+            publish_robot_description=True,
+            publish_robot_description_semantic=True,
+        )
+        .to_moveit_configs()
+    )
+
+    move_group_configuration = {
+        "use_sim_time": use_sim_time,
+        "allow_trajectory_execution": True,
+        "publish_robot_description_semantic": True,
+        "publish_planning_scene": True,
+        "publish_geometry_updates": True,
+        "publish_state_updates": True,
+        "publish_transforms_updates": True,
+        "monitor_dynamics": False,
+
+        # Zostawione jak w standardowym generate_move_group_launch:
+        # "capabilities": ParameterValue("", value_type=str),
+        "disable_capabilities": ParameterValue("", value_type=str),
+        "capabilities": "move_group/ExecuteTaskSolutionCapability",
+    }
+
+    robot_ns = LaunchConfiguration("robot_ns")
+
+    sensors_3d = load_yaml("velma_moveit_config", "config/sensors_3d.yaml")
+    octomap_config = {
+        "octomap_frame": "world",
+        "octomap_resolution": 0.05,
+        "max_range": 5.0,
+    }
+
+    move_group_node = Node(
+        package="moveit_ros_move_group",
+        executable="move_group",
+        namespace=robot_ns,
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            move_group_configuration,
+            sensors_3d,
+            octomap_config,
+        ],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="true",
+            description="Use simulation clock",
+        ),
+        DeclareLaunchArgument(
+                "robot_ns",
+                default_value="",
+                description="Robot namespace",
+            ),
+        move_group_node,
+    ])
--- velma_moveit_config/launch/moveit_rviz.launch.py
+++ velma_moveit_config/launch/moveit_rviz.launch.py
@@ -1,7 +1,56 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
 from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("velma", package_name="velma_moveit_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    rviz_config = LaunchConfiguration("rviz_config")
+
+    moveit_config = (
+        MoveItConfigsBuilder("velma", package_name="velma_moveit_config")
+        .trajectory_execution(
+            file_path="config/moveit_controllers.yaml",
+            moveit_manage_controllers=False,
+        )
+        .to_moveit_configs()
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],
+        parameters=[
+            moveit_config.planning_pipelines,
+            moveit_config.robot_description_kinematics,
+            moveit_config.joint_limits,
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.trajectory_execution,
+            {"use_sim_time": use_sim_time},
+        ],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="true",
+            description="Use simulation clock",
+        ),
+        DeclareLaunchArgument(
+            "rviz_config",
+            default_value=PathJoinSubstitution([
+                FindPackageShare("velma_moveit_config"),
+                "config",
+                "moveit.rviz",
+            ]),
+            description="RViz config file",
+        ),
+        rviz_node,
+    ])
